f37222e4ab0a6b9bfafe40b6e4547eb396802875
pmick <patrickmick1@gmail.com>
2018-05-18 20:12:39 -0700

    Initial Commit

---
+++ Twitch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Twitch
+//
+//  Created by Patrick Mick on 5/18/18.
+//  Copyright © 2018 Patrick Mick. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
