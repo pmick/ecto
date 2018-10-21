d4c52f2bd5649c4d301eb4cfdb3c2c84972d0af9
pmick <patrickmick1@gmail.com>
2018-10-21 16:32:48 -0700

    Cleanup folder structure

---
+++ Twitch/Support/StreamViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  HorizontalSectionController.swift
+//  Twitch
+//
+//  Created by Patrick Mick on 5/21/18.
+//  Copyright © 2018 Patrick Mick. All rights reserved.
+//
+
+import TwitchKit
+import IGListKit
+import UIKit
+
+typealias ChannelName = String
+typealias UserId = String
+typealias ChannelNameContext = Either<ChannelName, UserId>
+
+protocol ChannelNameProviding {
+    var context: ChannelNameContext { get }
+}
+
+extension LegacyStream: ChannelNameProviding {
+    var context: ChannelNameContext {
+        return .lhs(channel.name)
+    }
+}
+
+extension TwitchKit.Stream: ChannelNameProviding {
+    var context: ChannelNameContext {
+        return .rhs(userId)
+    }
+}
+
+final class StreamViewModel: ListDiffable {
+    let id: String
+    let title: String
+    let imageUrl: URL
+    
+    let stream: ChannelNameProviding
+    
+    init(featured: Featured) {
+        self.id = String(featured.stream.id)
+        self.title = featured.title
+        self.imageUrl = URL(string: featured.stream.preview.large)!
+        
+        self.stream = featured.stream
+    }
+    
+    init(stream: TwitchKit.Stream) {
+        self.id = stream.id
+        self.title = stream.title
+        // 640x360
+        let largePath = stream.thumbnailUrl.replacingOccurrences(of: "{width}", with: "640").replacingOccurrences(of: "{height}", with: "360")
+        self.imageUrl = URL(string: largePath)!
+        
+        self.stream = stream
+    }
+    
+    func diffIdentifier() -> NSObjectProtocol {
+        return id as NSObjectProtocol
+    }
+    
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        guard self !== object else { return false }
+        guard let object = object as? StreamViewModel else { return false }
+        return object.id == id &&
+            object.title == title &&
+            object.imageUrl == imageUrl
+    }
+}
